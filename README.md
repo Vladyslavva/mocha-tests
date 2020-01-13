04fc829ef6327d10f8e7b7e03b14bf0b5967ab6a
Vladyslavva <vladyslava.iurchenko@gmail.com>
2020-01-12 19:22:51 -0800

    iurchenko/mocha-test

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 ## Unit-testing
 
 **About**
-This project 
+This project provides examples of unit testing using [Mocha JavaScript framework](https://mochajs.org/) and  [Chai Assertion Library](https://www.chaijs.com/)
 
 **Project Layout**
+* _README.md_ describes the project and helps to install all necessary tools.
 * _test_ contains unit tests. The tests are written with the help of assertion library 'Chai', interface 'Expect' is used.
-* _.gitignore_ with idea and node_modules.
-* _index.js_ contains functions
-* _package-lock.json_ and _package.json_
+* _.gitignore_ (to exclude idea and node_modules from github).
+* _index.js_ contains functions.
+*  _package.json_ contains settings and dependencies.
+* _package-lock.json_ will be generated automatically.
+
 
 
 **Using the Project**
 To set up your environment you'll need to install:
 * git
 * Node.js
-* 
+* Mocha testing framework
+* Chai Assertion library
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function sum (a, b){
-    return a + b;
+  return a + b;
 }
 function mult (a, b){
-    return a * b;
+  return a * b;
 }
 function isAdult (age){
-    return age < 0 ? undefined : age >= 21;
+  return age < 0 ? undefined : age >= 21;
 }
 function sortArrayUp (arr){
-    return arr.sort((a, b) => (a - b));
+  return arr.sort((a, b) => (a - b));
 }
 function isPalindrome (str){
-    if(str === null || str === undefined) return false;
-    for(let i = 0; i < str.length/2; i++){
-        if(str[i] !== str[str.length - i - 1]) return false;
-    } return true;
+  if(str === null || str === undefined) return false;
+  for(let i = 0; i < str.length/2; i++){
+    if(str[i] !== str[str.length - i - 1]) return false;
+  } return true;
 }
 function isOddNumber (num){
   return num % 2 !== 0;
 }
 function isEvenNumber(num){
-    return num % 2 === 0;
+  return num % 2 === 0;
 }
 module.exports = {sum, mult, isAdult, sortArrayUp, isPalindrome, isOddNumber, isEvenNumber};
